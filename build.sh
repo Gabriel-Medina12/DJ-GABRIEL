0689bf2fd1eabad0e6c54c9415ac153178f962f9
Gabriel Medina <saulymarclash12@gmail.com>
2025-05-08 12:25:18 -0400

    Subiendo proyecto de bot de musica a servidor

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Instalar dependencias del sistema
+apt-get update && apt-get install -y ffmpeg
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python bot.py  # Reemplaza con el nombre de tu archivo .py
