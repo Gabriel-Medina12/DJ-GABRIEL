462cc2a8c236bdd01841cd08c17fa76ff6efe794
Gabriel Medina <saulymarclash12@gmail.com>
2025-05-08 12:54:35 -0400

    Cambios en el repositorio

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Instalar dependencias del sistema
-apt-get update && apt-get install -y ffmpeg
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python bot.py  # Reemplaza con el nombre de tu archivo .py
